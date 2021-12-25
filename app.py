ab55a14d6268a2383eca23e14c8a96dc60680ae7
Tala Hethnawi <hethnawi.tala@gmail.com>
2021-12-25 18:52:32 +0200

    removing unused imports

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, jsonify
 
-from catalog_service import CatalogService
-from models import Order
-from utils import session
-
 app = Flask(__name__)
 
 if __name__ == '__main__':
