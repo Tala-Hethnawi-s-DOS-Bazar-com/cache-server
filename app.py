fba18c25be2b9c81d05192a9e51baf666458c018
Tala Hethnawi <hethnawi.tala@gmail.com>
2021-12-25 20:23:38 +0200

    Setting up cache and load balancer

--- app.py
+++ app.py
@@ -1,6 +1,58 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from cache import cache
+from catalog_service import CatalogService
+from order_service import OrderService
 
 app = Flask(__name__)
 
+
+@app.route('/cache/<book_id>', methods=["DELETE"])
+def delete_cache(book_id):
+    try:
+        cache.delete(f"/info/{book_id}")
+    except:
+        pass
+    return jsonify({"message": "Cache deleted successfully"})
+
+
+@app.route('/purchase/<book_id>', methods=["POST"])
+def purchase(book_id):
+    try:
+        return jsonify(OrderService().purchase(book_id=book_id))
+    except Exception:
+        return jsonify({"error": "Sorry!, we were unable to process your order"}), 500
+
+@app.route('/search/<topic>', methods=["GET"])
+@cache.cached(key_prefix="%s")
+def search(topic):
+    try:
+        return jsonify(CatalogService().searchBooks(topic=topic))
+    except:
+        return jsonify({"error": "Failed to get results"}), 500
+
+
+@app.route('/info/<book_id>', methods=["GET"])
+@cache.cached(key_prefix="%s")
+def info(book_id):
+    # get the book with the requested id
+    try:
+        return jsonify(CatalogService().getBookInfo(book_id=book_id))
+    except:
+        return jsonify({"error": "Failed to get results"}), 500
+
+
+@app.route('/update/<book_id>', methods=["PUT"])
+def update(book_id):
+    # get the book with the requested id
+    request_body = request.json
+    try:
+        return jsonify(CatalogService().updateBookQuantity(book_id=book_id, quantity=request_body["quantity"]))
+    except:
+        return jsonify({"error": "Failed to update book"}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    cache.init_app(app)
+    cache.set("catalog_server", 0)
+    cache.set("order_server", 0)
---
+++ catalog_service.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from cache import cache
+
+
+class CatalogService:
+    __CATALOG_SERVER_URLS = ["http://172.18.0.20:5000/", "http://172.18.0.21:5000/"]
+    __INFO_ENDPOINT = "info/{book_id}"
+    __SEARCH_ENDPOINT = "search/{topic}"
+    __UPDATE_QUANTITY_ENDPOINT = "update/{book_id}"
+
+    def getBookInfo(self, book_id):
+        # get book details from Catalog MS
+        url = self.get_url() + self.__INFO_ENDPOINT.format(book_id=book_id)
+        response = requests.get(url=url)
+        if response.status_code >= 400:
+            raise Exception("Failed to get book info.")
+        return json.loads(response.content)
+
+    def updateBookQuantity(self, book_id, quantity):
+        # update quantity on catalog MS
+        url_1 = self.__CATALOG_SERVER_URLS[0] + self.__UPDATE_QUANTITY_ENDPOINT.format(book_id=book_id)
+        response = requests.put(url=url_1, json={"quantity": quantity})
+        if response.status_code >= 400:
+            raise Exception("Failed to update book.")
+        url_2 = self.__CATALOG_SERVER_URLS[1] + self.__UPDATE_QUANTITY_ENDPOINT.format(book_id=book_id)
+        response = requests.put(url=url_2, json={"quantity": quantity})
+        if response.status_code >= 400:
+            raise Exception("Failed to update book.")
+
+
+    def searchBooks(self, topic):
+        url = self.get_url() + self.__SEARCH_ENDPOINT.format(topic=topic)
+        response = requests.get(url=url)
+        if response.status_code >= 400:
+            raise Exception("Failed to search topic.")
+        return json.loads(response.content)
+
+    def get_url(self):
+        current_server = cache.get("catalog_server")
+        idx = current_server % 2
+        cache.set("catalog_server", current_server + 1)
+        return self.__CATALOG_SERVER_URLS[idx]
